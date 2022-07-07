a205d49b428cdf7a783e2b49c0f6a1bdf1589b66
LucSosa <l.sosamachado@gmail.com>
2022-07-07 15:24:31 -0300

    Fix: - Create a classe model, service, controller e service. - Page view init with bootstrap

--- src/main/java/com/spring/codeblog/CodeblogApplication.java
+++ src/main/java/com/spring/codeblog/CodeblogApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CodeblogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodeblogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CodeblogApplication.class, args);
+    }
 
 }
--- src/main/java/com/spring/codeblog/service/serviceImpl/CodeblogServiceImpl.java
+++ src/main/java/com/spring/codeblog/service/serviceImpl/CodeblogServiceImpl.java
@@ -1,2 +1,30 @@
-package com.spring.codeblog.service.serviceImpl;public class CodeblogServiceImpl {
+package com.spring.codeblog.service.serviceImpl;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.repository.CodeblogRepository;
+import com.spring.codeblog.service.CodeblogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CodeblogServiceImpl implements CodeblogService {
+    @Autowired
+    private CodeblogRepository codeblogRepository;
+
+    @Override
+    public List<Post> findAll() {
+      return codeblogRepository.findAll();
+    }
+
+    @Override
+    public Post findById(Long id) {
+        return codeblogRepository.findById(id).get();
+    }
+
+    @Override
+    public Post save(Post post) {
+        return codeblogRepository.save(post);
+    }
 }
